5a996c918ed87aab334f2077b3fe64d02d29161e
annia valdes <millerl.mb79@gmail.com>
2020-10-12 20:33:05 -0500

    fix removeBook function name in resolvers.js

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -61,11 +61,11 @@ const resolvers = {
     },
 
     removeBook: async (parent, { bookId }, context) => {
-
+     console.log('server remove',context.user._id,bookId)
       if (context.user) {     
         const remove = await User.findOneAndUpdate(
           { _id:context.user._id},
-          { $pull: { saveBooks: {bookId } } },
+          { $pull: { savedBooks: {bookId } } },
           { new: true }
         )
         return remove;
